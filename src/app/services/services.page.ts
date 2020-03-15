9cabfc7d89c03d2545b2d1e32984065f5b37b1b3
Auror007 <parmar.parth97531@gmail.com>
2020-03-16 01:20:28 +0530

    serv_veh

--- src/app/services/services.page.ts
+++ src/app/services/services.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonSlides, ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-services',
   templateUrl: './services.page.html',
@@ -11,11 +11,55 @@ export class ServicesPage implements OnInit {
   @ViewChild(IonSlides,{static: false}) slides: IonSlides;
   constructor(
     public actionSheetController: ActionSheetController,
-    public router:Router
+    public router:Router,
+    private http:HttpClient
     ) { }
 
   ngOnInit() {
+    const  vehicle:Array<
+      {
+        vehicleType:string,
+        brandName:string,
+        vehicleModel:string,
+        number:string,
+        vehicleCatagory:string
+      }>=[];
+  this.http.post<{
+    vehicle:Array<{vehicleType:string,brandName:string,vehicleModel:string,number:string,vehicleCatagory:string}>,
+    service:Array<{id:number,number:string}>,
+    package:Array<{name:string,duration:string,packageId:number}>
+  }>('https://mywash.herokuapp.com/uservehicle/getvehicle',{email:'meetp6041@gmail.com'}).subscribe((result)=>{
   
+  
+  console.log(result);
+   
+  result.vehicle.forEach(element => {
+    const ans1=result.service.find(x => x.number===element.number)
+    if(ans1!=undefined){
+      console.log(ans1);
+      
+        console.log(      
+          result.package.find(x=>x.packageId===ans1.id)
+        );
+      
+    }
+    
+    const a={
+      vehicleType:element.vehicleType,
+      brandName:element.brandName,
+      vehicleModel:element.vehicleModel,
+      number:element.number,
+      vehicleCatagory:element.vehicleCatagory
+    }
+    console.log(a);
+
+    vehicle.push(a);
+  });
+  
+  
+  });
+
+
   }
 
   gotoCart(){
